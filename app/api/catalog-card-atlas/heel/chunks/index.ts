7681090ca8dfc6ccae099679c32c1a7ad0646183
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-10 01:20:40 -0300

    Assemble heel card atlas

---
+++ app/api/catalog-card-atlas/heel/chunks/index.ts
@@ -0,0 +1,8 @@
+import h01 from "./h01";
+import h02 from "./h02";
+import h03 from "./h03";
+import h04 from "./h04";
+import h05 from "./h05";
+import h06 from "./h06";
+
+export const heelCardAtlasBase64 = [h01, h02, h03, h04, h05, h06].join("");
